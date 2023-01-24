0ecad1ff0fda97a17078e9bccd0492568344a023
Martin Skocik <mskocik@gmail.com>
2023-01-24 10:35:53 +0100

    update badges to reflect stable and dev version

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -85,7 +85,7 @@
     <a href="{base}/" class="decoration-transparent text-3xl lg:text-3" style="text-decoration: none">📅 Svelty Picker</a>
   </div>
   <div class="flex items-center text-2x">
-    <a class="mx-4" href="https://github.com/mskocik/svelty-picker" target="_blank">
+    <a class="mx-4" href="https://github.com/mskocik/svelty-picker" target="_blank" rel="noreferrer">
       <svg width="36px" height="36px" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32" data-v-8ff9b2e6=""><path fill="currentColor" fill-rule="evenodd" d="M16 2a14 14 0 0 0-4.43 27.28c.7.13 1-.3 1-.67v-2.38c-3.89.84-4.71-1.88-4.71-1.88a3.71 3.71 0 0 0-1.62-2.05c-1.27-.86.1-.85.1-.85a2.94 2.94 0 0 1 2.14 1.45a3 3 0 0 0 4.08 1.16a2.93 2.93 0 0 1 .88-1.87c-3.1-.36-6.37-1.56-6.37-6.92a5.4 5.4 0 0 1 1.44-3.76a5 5 0 0 1 .14-3.7s1.17-.38 3.85 1.43a13.3 13.3 0 0 1 7 0c2.67-1.81 3.84-1.43 3.84-1.43a5 5 0 0 1 .14 3.7a5.4 5.4 0 0 1 1.44 3.76c0 5.38-3.27 6.56-6.39 6.91a3.33 3.33 0 0 1 .95 2.59v3.84c0 .46.25.81 1 .67A14 14 0 0 0 16 2Z"></path></svg>
     </a>
     <button class="inline-flex text-xl p-2"  on:click={toggleTheme} aria-label="Switch theme">
@@ -115,7 +115,13 @@
           </li>
         </ul>
       </nav>
-      <a href="https://github.com/mskocik/svelty-picker"><img alt="GitHub package.json version" src="https://img.shields.io/github/package-json/v/mskocik/svelty-picker?logo=github&amp;logoColor=g"></a>
+      <div>
+        <a href="https://github.com/mskocik/svelty-picker"><img alt="GitHub package.json version" src="https://img.shields.io/github/package-json/v/mskocik/svelty-picker/main?label=version&logo=github&amp;logoColor=g"></a>
+      </div>
+      <div class="mt-2">
+        <a href="https://github.com/mskocik/svelty-picker"><img alt="GitHub package.json version (branch)" src="https://img.shields.io/github/package-json/v/mskocik/svelty-picker/v4.0?label=dev&logo=github"></a>
+      </div>
+      
     </div>
   </aside>
 	<main class="lg:px-6 lg:pt-6 w-full lg:w-4/5">
