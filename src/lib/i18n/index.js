cc4de1c5938404caf4d33b3675b8ed1dfcf88ab9
Jin Changhoon <susemi99@gmail.com>
2023-02-10 12:00:00 +0900

    Add Korean Translation

--- src/lib/i18n/index.js
+++ src/lib/i18n/index.js
@@ -118,6 +118,20 @@ export const sk = {
   backToDate:  'Späť na kalendár'
 }
 
+export const ko = {
+  days:        ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+  daysShort:   ['일', '월', '화', '수', '목', '금', '토', '일'],
+  daysMin:     ['일', '월', '화', '수', '목', '금', '토', '일'],
+  months:      ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  monthsShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  meridiem:    ['오전', '오후'],
+  suffix:      ['', '', '', ''],
+  todayBtn:    '오늘',
+  clearBtn:    '지우기',
+  timeView:    '시계보기',
+  backToDate:  '달력보기'
+}
+
 export default {
-  cz, de, en, es, id, jp, sk
+  cz, de, en, es, id, jp, sk, ko
 }
