2e104a3b6b7edbf5c652f1f5531e5a96e04fcef0
Martin Skočík <mskocik@gmail.com>
2023-07-14 15:03:40 +0200

    use floating-ui for calendar popup, close #80

--- src/lib/utils/utils.js
+++ src/lib/utils/utils.js
@@ -1,35 +1,46 @@
-// @ts-nocheck
+import { computePosition, autoUpdate, shift, flip } from '@floating-ui/dom';
 import { cubicOut } from 'svelte/easing';
 
-export function usePosition(node, { visible }) {
-  const onScrollHandler = () => visible && isOutOfViewport(node);
-  onScrollHandler();
-  document.addEventListener('scroll', onScrollHandler, { passive: true });
+/**
+ * @param {HTMLDivElement} node
+ * @returns void
+ */
+export function usePosition(node) {
+  if (node?.previousElementSibling === null) return;
+  /** @type Element */
+  const prevElement = node?.previousElementSibling;
+  const removeFloating = autoUpdate(prevElement, node, () => 
+    computePosition(prevElement, node, {
+      placement: 'bottom-start',
+      middleware: [
+        shift({
+          padding: 5
+        }),
+        flip()
+      ]
+    }).then(({x, y}) => {
+      Object.assign(node.style, {
+        left: `${x}px`,
+        top: `${y}px`,
+      });
+    })
+  )
 
   return {
-    update(newVal) {
-      visible = newVal;
-    },
     destroy() {
-      document.removeEventListener('scroll', onScrollHandler, { passive: true });
+      removeFloating();
     }
   }
 }
 
-/** slightly addapted from mskocik/svelecte */
-function isOutOfViewport(elem) {
-  if (!elem) return false;
-  const parentBounding = elem
-    .parentElement  // wrapper
-    .getBoundingClientRect();
-  const bounding = elem.getBoundingClientRect();
-
-  elem.style.bottom = parentBounding.bottom + bounding.height > (window.innerHeight || document.documentElement.clientHeight)
-    ? elem.previousElementSibling.clientHeight + 'px'
-    : '';
-};
-
-export function scale(node, { delay = 0, duration = 400, easing = cubicOut, start = 0, end = 1, opacity = 0 } = {}) {
+/**
+ * 
+ * @param {HTMLElement} node 
+ * @param {import('svelte/transition').ScaleParams} params
+ * @returns {import('svelte/transition').TransitionConfig}
+ */
+export function scale(node, { delay = 0, duration = 400, easing = cubicOut, start = 0, opacity = 0 } = {}) {
+  const end = 1;
   const style = getComputedStyle(node);
   const target_opacity = +style.opacity;
   const transform = style.transform === 'none' ? '' : style.transform;
@@ -39,7 +50,7 @@ export function scale(node, { delay = 0, duration = 400, easing = cubicOut, star
       delay,
       duration,
       easing,
-      css: (_t, u) => `
+      css: (/** @type number */ _t, /** @type number */ u) => `
         transform: ${transform} scale(${end !== 1 ? start + end * u : 1 - (sd * u)});
         opacity: ${target_opacity - (od * u)};
       `
