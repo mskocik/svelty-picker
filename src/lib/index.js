0ac9cecbdd5e1a696d88ec10dc26de58858b9891
Martin Skočík <mskocik@gmail.com>
2023-07-26 18:02:25 +0200

    separate custom-element export

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,4 +4,3 @@ import Component, { config } from './components/SveltyPicker.svelte';
 export default Component;
 export { config };
 export { parseDate, formatDate } from './utils/dateUtils.js';
-export { registerSveltyPicker } from './utils/custom-element';
