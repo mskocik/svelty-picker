8feb23dd6f0c5cf7ed87b77ef2a5ca6fe6b0ba2d
Martin Skočík <mskocik@gmail.com>
2024-02-21 14:03:55 +0100

    v5.2.4

---
+++ docs/_app/immutable/chunks/stores.23fc378a.js
@@ -0,0 +1,1 @@
+import"./scheduler.8d4e2d99.js";import{d as r}from"./singletons.6475323f.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},a={subscribe(s){return e().page.subscribe(s)}},o={subscribe(s){return e().navigating.subscribe(s)}};export{o as n,a as p};
--- docs/_app/immutable/nodes/4.4d7fc079.js
+++
@@ -1,1 +0,0 @@
-import{au as e,at as n}from"../chunks/4.f3c9949e.js";export{e as component,n as universal};
